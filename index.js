0c53438aed8780a8fe96f6c79c6e42bf5baeec07
unknown <najmiddinnasriddinov5@gmail.com>
2024-04-12 19:53:06 +0500

    3-rd done

--- index.js
+++ index.js
@@ -4,3 +4,17 @@
 // }
 
 // console.log(sum(3, 5));
+
+
+        // 3-MASALA
+// let str = `radar`;
+
+// let isPalindrome = () => {
+//     if (Array.from(str).reverse().join('') == str) {
+//         return `This word is palindrome`
+//     } else {
+//         return `This word isn't palindrome`
+//     }
+// }
+
+// console.log(isPalindrome())
